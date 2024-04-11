91e07dae8b48a7f52cbd030ea2fdff769fa3ffca
Esteban Cáceres <estebancacerest@gmail.com>
2024-04-11 14:42:54 -0400

    creamos queries

--- consultas/consultas.js
+++ consultas/consultas.js
@@ -1,1 +1,32 @@
 import db from "../config/db.js"
+
+//agregar datos
+const agregarRutinas = async() => {
+    try{   
+        const consultaRutinas = {
+            text: '',
+            values: '',
+        }
+    }catch(err){
+        console.log(err.message)
+    }
+}
+
+//ver rutinas
+const verRutinas = async () => {
+    try{
+            const consultaRutinas = {
+                text: "select * from ejercicios"
+            }
+            const result = await db.query(consultaRutinas);
+            console.log(result.rows);
+            return result.rows
+    }catch(err){
+        console.log(err.message);
+    }
+}
+
+export { 
+    agregarRutinas,
+    verRutinas
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,8 @@
 import express from 'express';
 //path para realizar la config del dirname
 import path from 'path'
+//importamos la queries
+import { agregarRutinas, verRutinas} from '../consultas/consultas.js'
 const router = express.Router();
 //dirname para hacer publica la carpeta de assets
 const __dirname = import.meta.dirname
@@ -9,6 +11,13 @@ const __dirname = import.meta.dirname
 //trbajamos el link con dirname, para enlazar index con el html
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../views/index.html'))
+})
+
+
+//creamos ruta para las consultas
+router.get('/rutinas',async (req, res) => {
+    const mostrarRutinas = await verRutinas();
+    res.json(mostrarRutinas);
 })
 
 
